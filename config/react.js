e5c3b6af85d7ff1e004a2182ce1ee22a3ac25119
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2025-10-05 23:46:30 +0100

    Updates of react-hooks rules

--- config/react.js
+++ config/react.js
@@ -125,7 +125,22 @@ export default defineConfig([{
     // Rules provided by eslint-plugin-react-hooks; they should be applied to
     // all files, as hooks do not have to live exclusively in .jsx / .tsx files
     // alongside JSX syntax!
+    'react-hooks/component-hook-factories': 'error',
+    'react-hooks/config': 'error',
+    'react-hooks/error-boundaries': 'error',
     'react-hooks/exhaustive-deps': 'error',
+    'react-hooks/gating': 'error',
+    'react-hooks/globals': 'error',
+    'react-hooks/immutability': 'error',
+    'react-hooks/incompatible-library': 'error',
+    'react-hooks/preserve-manual-memoization': 'error',
+    'react-hooks/purity': 'error',
+    'react-hooks/refs': 'error',
     'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/set-state-in-effect': 'error',
+    'react-hooks/set-state-in-render': 'error',
+    'react-hooks/static-components': 'error',
+    'react-hooks/unsupported-syntax': 'error',
+    'react-hooks/use-memo': 'error',
   },
 }]);
